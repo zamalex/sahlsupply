b606ca5efc956a4b943bc1325297e55254f176da
user244009 <mustafaezzaldin@outlook.com>
2023-09-19 01:37:37 -0700

    ios release 19-9

---
+++ build/app/generated/source/buildConfig/release/com/sahlsupplyapp/marketplace/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sahlsupplyapp.marketplace;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.sahlsupplyapp.marketplace";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 3001;
+  public static final String VERSION_NAME = "1.0.0";
+}
